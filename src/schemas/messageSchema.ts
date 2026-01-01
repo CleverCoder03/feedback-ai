999ab62a15f231dd1b272a09648583e8adc4d53c
Senpai <clevercoder0307@gmail.com>
2026-01-01 14:04:00 +0530

    implement zod validation schemas

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,5 @@
+import {z} from "zod"
+
+export const messageSchema = z.object({
+    content: z.string().min(5, {message: "Content must be at least 5 characters"}).max(300, {message: "Content must be no longer than 300 characters"})
+})
